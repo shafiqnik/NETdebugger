9ae4cb026ae1982e65f991e9eacb4ea7616a2a91
shafiqnik <shafiq.ahmad@gmail.com>
2022-06-30 09:33:37 -0700

    GUI has two text fields one for beacon id, another for day. The code goes through audit log, analyzes the audit log and provides easy to understand diagnostic feedback to the user. The user doesn't have to interpret the audit log and manually check each value and parameters.

--- NetChecker.py
+++ NetChecker.py
@@ -1,7 +1,10 @@
+#https://www.pythontutorial.net/tkinter/
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 from tkinter.ttk import Entry
+from tkcalendar import DateEntry
 
 from AuditLog import Audit
 
@@ -34,8 +37,6 @@ def login_clicked():
    # print('result.......',result)
     displayMessage = str(result) +'\n'+ str(text2)+' \n'+str(ble_tag)+'\n'+str(powerIssue)
 
-
-
     feedback = ttk.Label(signin, text=displayMessage)
     feedback.pack(ipadx=50, ipady=50)
 
@@ -49,34 +50,41 @@ def login_clicked():
     '''
 
 
-# Sign in frame
+
+# Create gui frame
 signin = ttk.Frame(root)
 signin.pack(ipadx=10, ipady=10)
 
 
-# email
-email_label = ttk.Label(signin, text="Beacon ID")
-email_label.pack(ipadx=5, ipady=5)
-
-email_entry: Entry = ttk.Entry(signin, textvariable=bid)
-email_entry.pack()
-email_entry.focus()
-
+'''
+#calendar  https://www.plus2net.com/python/tkinter-DateEntry.php
+sel=tk.StringVar() # declaring string variable
+cal=DateEntry(root,selectmode='day',textvariable=sel)
+cal.pack()
+'''
 
 
+# Enter beaconID
+beaconID = ttk.Label(signin, text="Beacon ID")
+beaconID.pack(ipadx=5, ipady=5)
+beaconID_entry: Entry = ttk.Entry(signin, textvariable=bid)
+beaconID_entry.pack()
+beaconID_entry.focus()
 
-# password
-password_label = ttk.Label(signin, text="Day")
-password_label.pack(ipadx=5,ipady=5, expand=True)
 
 
-password_entry = ttk.Entry(signin, textvariable=date)
-password_entry.pack( expand=True)
 
-# password
-password_label = ttk.Label(signin, text="Example June 15th, enter 15. "
+
+# Enter day
+day = ttk.Label(signin, text="Day")
+day.pack(ipadx=5,ipady=5, expand=True)
+day_entry = ttk.Entry(signin, textvariable=date)
+day_entry.pack( expand=True)
+#label
+day_label = ttk.Label(signin, text="Example June 15th, enter 15. "
                                         "The tool will use current month" )
-password_label.pack(ipadx=5,ipady=5, expand=True)
+day_label.pack(ipadx=5,ipady=5, expand=True)
+
 # login button
 login_button = ttk.Button(signin, text="Check Audit Log", command=login_clicked)
 login_button.pack(expand=True, pady=20)
