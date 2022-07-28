6833cec42cc366203ba4ea36207cd0c972402b18
shafiqnik <shafiq.ahmad@gmail.com>
2022-07-28 16:31:35 -0700

    GUI has two text fields one for beacon id, another for selecting dates from the calendar The code goes through audit log, analyzes the audit log and provides easy to understand diagnostic feedback to the user. The user doesn't have to interpret the audit log and manually check each value and parameters. it also stores the results in locall db as json file

---
+++ NETdebugger.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'NETdebugger.exe' with Python DLL being found.
+set PATH=c:\PROGRA~3\ANACON~1\envs\fastapi;%PATH%
+"%~dp0.\NETdebugger.exe"
+
